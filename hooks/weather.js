10d7f04d9e6678e90253031db32bf4c78b167e38
Nilmar Sanchez Muguercia <nilmar@uci.cu>
2021-03-05 19:24:10 -0500

    Realizando la peticion del tiempo de manera segura

--- hooks/weather.js
+++ hooks/weather.js
@@ -6,7 +6,7 @@ function useApi(initial) {
   //debe estar en un archivo aparte
   async function api(city, country) {
     const apiKey = "e4bfd2e82565376da9672644df941162";
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`;
 
     let response = await fetch(url);
     let jsonResponse = await response.json();
